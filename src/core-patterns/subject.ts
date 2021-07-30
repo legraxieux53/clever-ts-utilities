70aa19dc58b5ae38540dbdd7fb51592f744c005e
nehemie.konan <nehemie.konan@inexa-ci.com>
2021-07-30 15:06:25 +0000

    fix: bug subject

--- src/core-patterns/subject.ts
+++ src/core-patterns/subject.ts
@@ -1,7 +1,7 @@
 import { Observer } from './observer';
 import { Collection } from './collection';
 export class Subject<T> {
-    constructor(private id: string, private observerCollection: Collection<Observer<T>>) {}
+    constructor(private id: string, private observerCollection: Collection<Observer<T>>) { }
 
     registerObserver(obs: Observer<T>) {
         this.observerCollection.add(obs);
@@ -15,7 +15,10 @@ export class Subject<T> {
         const iterator = this.observerCollection.createIterator();
         iterator.reset();
         while (iterator.hasMore()) {
-            iterator.getNext().notify(this.id, data);
+            const next = iterator.getNext();
+            if (next) {
+                next.notify(this.id, data);
+            }
         }
     }
 }
