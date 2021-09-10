2f77125e10ba3569f4abf4ecd984e640fcce648e
nehemie konan <nehemiekonan535@gmail.com>
2021-09-10 22:22:25 +0000

    fix(types): Correction matching types

--- src/strings/is-string-undefined-or-null.ts
+++ src/strings/is-string-undefined-or-null.ts
@@ -1,3 +1,3 @@
-export function isStringUndefinedOrNullOrEmpty(data: string): boolean {
+export function isStringUndefinedOrNullOrEmpty(data: string | String): boolean | Boolean {
     return data === undefined || data === null || data.replace(/\s/g, '') === '';
 }
