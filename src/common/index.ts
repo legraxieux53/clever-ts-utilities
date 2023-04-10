aefbf4b04cb537605cc1cb787733f6bc0096271b
Nehemie Konan <nehemiekonan535@gmail.com>
2023-04-10 12:06:38 +0000

    fix: Loading responsive dialog width

--- src/common/index.ts
+++ src/common/index.ts
@@ -3,3 +3,4 @@ export * from "./is-undefined-or-null";
 export * from "./copy";
 export * from "./get-object-by-field-value";
 export * from "./objects-equals";
+export * from "./responsive-dialog-width";
