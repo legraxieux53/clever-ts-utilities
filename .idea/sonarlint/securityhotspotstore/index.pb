97abdbc941b4b32a943836183779237e0e0dcd68
legracieux53 <nehemiekonan535@gmail.com>
2024-09-26 16:58:24 +0000

    update mapper

---
+++ .idea/sonarlint/securityhotspotstore/index.pb
@@ -0,0 +1,25 @@
+
+9
+	README.md,8\e\8ec9a00bfd09b3190ac6b22251dbb1aa95a0579d
+W
+'src/clean-architecture-base/i-mapper.ts,7\f\7f5fb0c291ac3f7bd87f0386de08675ce3e46063
+^
+.src/clean-architecture-base/i-query.handler.ts,9\4\94fd21d3883126a39fb172e7a6f4ecf89b494bf9
+g
+7src/clean-architecture-base/i-tenant-command.handler.ts,8\2\82a8cbbea556fad4913fe2f2fb5879a0e28eedeb
+e
+5src/clean-architecture-base/i-tenant-query.handler.ts,2\c\2c57056991a642da89b227f83078ca3d1d61f0db
+T
+$src/clean-architecture-base/index.ts,3\6\3641263b178187be930f9bc901af55a984a0112f
+Z
+*src/clean-architecture-base/auto-mapper.ts,6\a\6a0ade49ae9b06e69826abe671c1f8568286aadc
+Y
+)src/clean-architecture-base/base-model.ts,8\f\8f04829a041c1458803d7c88d254bb9cbaabf2b8
+`
+0src/clean-architecture-base/i-command.handler.ts,8\1\81a6bfdecd9d72cd88f31506888b64bd7c617bbf
+X
+(src/clean-architecture-base/i-usecase.ts,3\4\34fa3152cac3fcfa1da9ade9543fec93ed5353d4
+g
+7src/clean-architecture-base/base-model-without-props.ts,5\c\5c125d371e4bcb75fdb42ead4d01687dbacd1620
+<
+package.json,7\0\7030d0b2f71b999ff89a343de08c414af32fc93a
--- src/clean-architecture-base/i-mapper.ts
+++ src/clean-architecture-base/i-mapper.ts
@@ -6,7 +6,7 @@ export interface IMapFrom<I, O> {
      * @description Map from domain to data layer
      * @param param
      */
-    mapFrom(param: I): O | Promise<O>;
+    mapFrom(param: I): O;
 }
 
 /**
@@ -17,7 +17,7 @@ export interface IMapTo<I, O> {
      * @description Map from data to domain layer
      * @param param
      */
-    mapTo(param: O): I | Promise<I>;
+    mapTo(param: O): I;
 }
 
 /**
@@ -31,3 +31,37 @@ export interface IMapTo<I, O> {
  * @template O
  */
 export interface IMapper<I, O> extends IMapFrom<I, O>, IMapTo<I, O>{}
+
+/**
+ * @description Interface for mapping between domain and data layer
+ */
+export interface IMapFromAsync<I, O> {
+    /**
+     * @description Map from domain to data layer
+     * @param param
+     */
+    mapFrom(param: I): Promise<O>;
+}
+
+/**
+ * @description Interface for mapping between domain and data layer
+ */
+export interface IMapToAsync<I, O> {
+    /**
+     * @description Map from data to domain layer
+     * @param param
+     */
+    mapTo(param: O): Promise<I>;
+}
+
+/**
+ * @description Mapper interface for mapping between domain and data layer
+ * @date 22/03/2023 - 11:24:26
+ *
+ * @export
+ * @interface Mapper
+ * @typedef {Mapper}
+ * @template I
+ * @template O
+ */
+export interface IMapperAsync<I, O> extends IMapFromAsync<I, O>, IMapToAsync<I, O>{}
